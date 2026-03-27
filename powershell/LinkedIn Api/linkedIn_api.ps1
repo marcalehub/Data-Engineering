90e3b498ea7a9ae3ad3735c964a980dcef11edf3
Marcos De Vargas <marcosalevargas@icloud.com>
2026-03-27 11:31:09 -0400

    redefine LinkedIn API Comunication

--- powershell/LinkedIn Api/linkedIn_api.ps1
+++ powershell/LinkedIn Api/linkedIn_api.ps1
@@ -1,56 +1,76 @@
-$listener = New-Object System.Net.HttpListener
-$listener.Prefixes.Add("$($Env:linkedIn_host)/callback/")
-$listener.Start()
-
-Start-Process "https://api.linkedin.com/oauth/v2/authorization?response_type=code&client_id=$($Env:linkedIn_client)&redirect_uri=$($Env:linkedIn_host)/callback&scope=$($Env:linkedIn_mscope)"
-
-$context = $listener.GetContext()
-$request = $context.Request
-$code = $request.QueryString["code"]
-$listener.Stop()
-
-$payLoad = @{
-    grant_type = "authorization_code"
-    redirect_uri  = "$($Env:LinkedIn_host)/callback"
-    client_id = $($Env:linkedIn_client)
-    client_secret = $($Env:linkedIn_secrect)
-    code = $($code)
+Function Get-Response {
+    $listener = New-Object System.Net.HttpListener
+    $listener.Prefixes.Add("$($Env:linkedIn_host)/callback/")
+    $listener.Start()
+    
+    Start-Process "https://api.linkedin.com/oauth/v2/authorization?response_type=code&client_id=$($Env:linkedIn_client)&redirect_uri=$($Env:linkedIn_host)/callback&scope=$($Env:linkedIn_mscope)"
+    
+    $context = $listener.GetContext()
+    $request = $context.Request
+    $code = $request.QueryString["code"]
+    $listener.Stop()
+    return $code
 }
+Function Get-AccessToken {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$code
+    )
+    $payLoad = @{
+        grant_type = "authorization_code"
+        redirect_uri  = "$($Env:LinkedIn_host)/callback"
+        client_id = $($Env:linkedIn_client)
+        client_secret = $($Env:linkedIn_secrect)
+        code = $($code)
+    }
 
-$accessToken = Invoke-WebRequest `
-    -Uri "https://api.linkedin.com/oauth/v2/accessToken" `
-    -Body $payLoad `
-    -Method Post `
-    -ContentType "application/x-www-form-urlencoded" `
-    -UseBasicParsing
+    $accessToken = Invoke-WebRequest `
+        -Uri "https://api.linkedin.com/oauth/v2/accessToken" `
+        -Body $payLoad `
+        -Method Post `
+        -ContentType "application/x-www-form-urlencoded" `
+        -UseBasicParsing
 
-$content = $accessToken.Content | ConvertFrom-Json
+    $content = $accessToken.Content | ConvertFrom-Json
+    $token = $content.access_token
+    return $token
 
-$header = @{
-    Authorization = "Bearer $($content.access_token)"
-    "X-Restli-Protocol-Version" = "2.0.0"
-    "LinkedIn-Version" = "202402"
-  }
+}
+Function Publish-BusinessFeed {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$token,
 
-$publish = @{
-    author = "urn:li:organization:$($Env:linkedIn_user)"
-    lifecycleState = "PUBLISHED"
-    specificContent = @{
-        "com.linkedin.ugc.ShareContent" = @{
-            "shareCommentary" = @{
-                text = "Hello World! This is my first Share on LinkedIn with PowerShell! Code will be available on GitHub soon."
+        [Parameter(Mandatory=$true)]
+        [string]$message
+
+    )
+    $header = @{
+        Authorization = "Bearer $token"
+        "X-Restli-Protocol-Version" = "2.0.0"
+        "LinkedIn-Version" = "202402"
+    }
+
+    $publish = @{
+        author = "urn:li:organization:$($Env:linkedIn_user)"
+        lifecycleState = "PUBLISHED"
+        specificContent = @{
+            "com.linkedin.ugc.ShareContent" = @{
+                "shareCommentary" = @{
+                    text = "$message"
+                }
+                shareMediaCategory = "NONE"
             }
-            shareMediaCategory = "NONE"
+        }
+        visibility = @{
+            "com.linkedin.ugc.MemberNetworkVisibility" = "PUBLIC"
         }
     }
-    visibility = @{
-        "com.linkedin.ugc.MemberNetworkVisibility" = "PUBLIC"
-    }
-}
 
-Invoke-WebRequest `
-    -Uri "https://api.linkedin.com/v2/ugcPosts" `
-    -Headers $header `
-    -Body ($publish | ConvertTo-Json -Depth 10) `
-    -Method Post `
-    -ContentType "application/json"
+    Invoke-WebRequest `
+        -Uri "https://api.linkedin.com/v2/ugcPosts" `
+        -Headers $header `
+        -Body ($publish | ConvertTo-Json -Depth 10) `
+        -Method Post `
+        -ContentType "application/json"
+}
