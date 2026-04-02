f2acfd144d1b4ba2ef03abea1c7a686d9d6c7d8b
Marcos De Vargas <marcosalevargas@icloud.com>
2026-04-02 17:13:56 -0400

    Adding PowerShell Script Commom Data Type, read me modi

---
+++ powershell/Management/datatype.ps1
@@ -0,0 +1,24 @@
+<# PowerShell Data Types#>
+
+#Basic data types in PowerShell include:
+[String] # String data type
+[SecureString] # Secure string data type for sensitive information
+[Int]  -or [Int64]  # Integer data type
+[Double] # Double-precision floating-point data type
+[Decimal] # Decimal data type
+[Bool]   # Boolean data type
+[Char]   # Character data type
+
+# Complex data types in PowerShell include:
+[Array]  # Array data type
+[DateTime] # Date and Time data type
+[Hashtable] # Hashtable data type
+[PSObject] # PowerShell Object data type
+[Object] # Generic Object data type
+
+# Special data types in PowerShell include:
+[Null] # Represents a null value
+[Void] # Represents the absence of a return value
+[ScriptBlock] # Represents a block of script code
+[System.IO.FileInfo] # Represents a file in the file system
+[System.Uri] # Represents a Uniform Resource Identifier (URI)
