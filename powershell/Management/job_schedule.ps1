c1b28a005b288e852fc9cbc2cd25bc37bdf04f4d
Marcos De Vargas <marcosalevargas@icloud.com>
2026-04-09 10:52:12 -0400

    Job Schedule full sample

--- powershell/Management/job_schedule.ps1
+++ powershell/Management/job_schedule.ps1
@@ -1,1 +1,3 @@
-Register-ScheduledJob -Name "JOB_NAME" -FilePath "FILE_PATH.ps1" 
+$opt = New-ScheduledJobOption -RequireNetwork -RunElevated -WakeToRun
+$trigger = New-JobTrigger -Weekly -DaysOfWeek Monday,Thursday -At '3:00PM'
+Register-ScheduledJob -Name "JOB_NAME" -FilePath "FILE_PATH.ps1"  -Trigger $trigger -ScheduledJobOption $opt
